3d2df253ad0f819aa4077d1026ed1434a48cd400
Melissa McEwen <mgmcewen@gmail.com>
2024-12-11 10:51:56 -0600

    refactoring types

--- src/index.ts
+++ src/index.ts
@@ -2,31 +2,4 @@ export { Analyzer } from './utils/analyzer';
 export { matchIngredient } from './utils/matcher';
 export { normalizer } from './utils/normalizer';
 
-export type {
-  // Core types
-  AnalyzerConfig,
-  AnalysisResult,
-
-  // Ingredient types
-  Ingredient,
-  IngredientMatch,
-  IngredientDatabase,
-
-  // Category types
-  Category,
-  CategoryGroup,
-  CategoryGroups,
-
-  // Match types
-  MatchType,
-  MatchSearch,
-  MatchConfig,
-  MatchDetails,
-  MatchOptions,
-
-  // Debug types
-  DebugInfo,
-
-  // Normalizer types
-  NormalizedIngredientList,
-} from '@/types';
+export * from './types';
